6d2f55057424ee0e8d55679d286ad0794e4f47ef
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-11-07 16:22:40 +0000

    sets up basic/simple password env variable auth for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_action :authenticate
   protect_from_forgery with: :exception
+
+  def authenticate
+    unless Rails.env.development? || Rails.env.testing?
+      authenticate_or_request_with_http_basic('Administration') do |username, password|
+        username == Figaro.env.username && password == Figaro.env.custom_login_key
+      end
+    end
+  end
 end
